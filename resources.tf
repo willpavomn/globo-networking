6153b0250090376c0775308878a88d6e30b24170
wpavon@arrow.com <wpavon@arrow.com>
2026-08-19 15:39:04 -0400

    Fix formatting

--- resources.tf
+++ resources.tf
@@ -17,7 +17,7 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    Environment = var.environmemt    
+    Environment = var.environmemt
   }
 }
 
